e4700ffd8879be573e7dc7d44bafb8d51e668057
Uneven Coder <ameenshafeeq2005@gmail.com>
2020-09-19 15:35:24 +0300

    trial for deployng

--- pokemon-game/src/server.js
+++ pokemon-game/src/server.js
@@ -2,11 +2,13 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-
+const express = require('express')
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-
-polka() // You can also use Express
+const app = express()
+export default app
+/*
+export default polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
@@ -15,3 +17,4 @@ polka() // You can also use Express
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
+*/
